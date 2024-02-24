de8d7334c37c24bec88a9cc5a3a3cf7b5b5d8798
Ethan Wolff <thesisyphean256@gmail.com>
2024-02-24 18:18:44 +0200

    Concurrent users using multiprocessing

--- client.py
+++ client.py
@@ -1,6 +1,18 @@
 import argparse
 import socket
 
+class Client:
+    def __init__(self, args):
+        self.username = args.username
+        self.ip_address = args.ip_address
+        self.port = args.port
+
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((self.ip_address, self.port))
+            print("Connected to the server!")
+            sock.sendall(self.username.encode())
+
 def main():
     parser = argparse.ArgumentParser(
         prog="ChatApp",
@@ -20,13 +32,8 @@ def main():
 
     args = parser.parse_args()
 
-    connect(args)
-
-def connect(args):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((args.ip_address, args.port))
-        print("Connected to the server!")
-        sock.sendall(args.username.encode())
+    client = Client(args)
+    client.run()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,22 +1,40 @@
 import socket
+from multiprocessing import Process
 
-HOST = "127.0.0.1"
-PORT = 65432
+class User(Process):
+    def __init__(self, conn, addr):
+        super().__init__()
+        self.conn = conn
+        self.addr = addr
+    
+    def run(self):
+        with self.conn:
+            self.username = self.conn.recv(50)
+            print(f"Connected by {self.addr}, Username: {self.username}")
+
+class Server:
+    # This IP adress is standard for the loopback interface
+    # Only processes on the host will be able to connect to the server
+    HOST = "127.0.0.1"
+    PORT = 65432
+
+    def __init__(self):
+        self.users = []
+
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind((self.HOST, self.PORT))
+            sock.listen()
+
+            while True:
+                conn, addr = sock.accept()
+                user = User(conn, addr)
+                self.users.append(user)
+                user.start()
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        # This IP adress is standard for the loopback interface
-        # Only processes on the host will be able to connect to the server
-        sock.bind((HOST, PORT))
-
-        sock.listen()
-
-        while True:
-            conn, addr = sock.accept()
-
-            with conn:
-                username = conn.recv(2048)
-                print(f"Connected by {addr}, Username: {username}")
+    server = Server()
+    server.run()
 
 if __name__ == "__main__":
     main()

4542e92c333a502b95494a6887655047731e66c7
LukeEberhard03 <luke.eberhard@gmail.com>
2024-02-29 16:59:55 +0200

    Co-authored-by: Ethan Wolff <thesisyphean@users.noreply.github.com> Co-authored-by: Colin Gale <CocoisBuggy@users.noreply.github.com>

--- lukes_client.py
+++ lukes_client.py
@@ -1,5 +1,6 @@
 import argparse
 import socket
+from enum import Enum
 
 # TODO: Cut these down
 class Colors:
@@ -12,6 +13,15 @@ class Colors:
     END = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+class Command(Enum):
+    ACCEPT_CONNECTION = 0
+    SEND_USERNAME = 1
+    ACCEPT_USERNAME = 2
+    DECLINE_USERNAME = 3
+    REQUEST_USER_LIST = 4
+    REQUEST_PTP_CONNECTION = 5
+
 
 def colour(message, colour):
     return colour + message + Colors.END
@@ -30,14 +40,38 @@ class Client:
     #         self.sock.sendall(...) # TODO: Protocol for setting 
 
     def run(self):
+        port = None
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((self.server_ip_address, self.server_port))
             print("Connected to the server!")
+            port = int.from_bytes(sock.recv(2), 'little')
+            print(f"Port: {port}")
+       
 
-            self.sock = sock
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((self.server_ip_address, port))
+            # self.sock = sock
             command_val = 1
             sock.sendall(b'\x01' + b'\x01' + "aaaaaaaa".encode('utf-8') + b'\x00'*2)
+            
             # self.set_usernme()
+
+            while True:
+                command_bytes = sock.recv(12)
+                self.process_command(command_bytes)
+
+     # takes input of command bytes
+    # 1 byte for command type, 8 bytes for param 1, 2 bytes for param 2
+    # returns command for reply
+    def process_command(self, command_bytes):
+        command_type = int.from_bytes(command_bytes[:1], 'big')
+        # these indices are subject to change based on username length restrictions and encoding
+        param_1 = command_bytes[1:9]
+        param_2 = command_bytes[9:11]
+        print(command_type)
+        if command_type == Command.ACCEPT_USERNAME.value:
+            print("hey hey hey")
+
 
 def main():
     parser = argparse.ArgumentParser(
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 from multiprocessing import Process
+from threading import Thread
 from enum import Enum
 import itertools
 
@@ -12,10 +13,10 @@ class Command(Enum):
     REQUEST_PTP_CONNECTION = 5
 
 class User(Process):
-    def __init__(self, conn, addr):
+    def __init__(self, sock):
         super().__init__()
-        self.conn = conn
-        self.addr = addr
+        self.sock = sock
+        # self.server = server
 
         self.username = ""
         self.available = False
@@ -27,21 +28,19 @@ class User(Process):
     # 1 byte for command type, 8 bytes for param 1, 2 bytes for param 2
     # returns command for reply
     def process_command(self, command_bytes):
-        command_type = int.from_bytes(command_bytes[:1], 'big')
+        command_type = command_bytes[0]
         # these indices are subject to change based on username length restrictions and encoding
         param_1 = command_bytes[1:9]
         param_2 = command_bytes[9:11]
-        
-        # print(Command.SEND_USERNAME)
-        # bruh wtf why is Command.SEND_USERNAME not evaluating to 1
-        # I may be using the enum wrong lmao, but I don't know why
-        if command_type == Command.SEND_USERNAME:
+
+        if command_type == Command.SEND_USERNAME.value:
             name = param_1.decode('utf-8')
             print(name)
-            for user in server.users:
-                if user.username == name:
-                    # return username taken message
-                    return
+            # print(self.server.users)
+            # for user in self.server.users:
+            #     if user.username == name:
+            #         # return username taken message
+            #         return
             self.username = name
             self.available = True
             if int.from_bytes(param_2, 'big') == 1:
@@ -50,18 +49,14 @@ class User(Process):
                 # send visible users list? May be simpler to just keep that as a separate 
                 # command and have the client side automatically request it on connection
 
-            return self.send_command(Command.ACCEPT_USERNAME)
+            return self.send_command(Command.ACCEPT_USERNAME.value)
 
-        if command_type == Command.REQUEST_USER_LIST: 
-            users = ""
-            for user in server.users:
-                if user.available and not user.busy:
-                    users += user
-                    users += ", "
-            users = users[:-1]
+        if command_type == Command.REQUEST_USER_LIST.value:
+            pass
+            # users = self.server.get_users()
             # generate data transfer message with user list and return
 
-        if command_type == Command.REQUEST_PTP_CONNECTION:
+        if command_type == Command.REQUEST_PTP_CONNECTION.value:
             pass
 
 
@@ -74,14 +69,17 @@ class User(Process):
     # this is fun lmao
     # pads params 1 and 2 with 0s by default, otherwise assumes values if inputed are padded
     def send_command(self, command_num, param_1=b'\x00'*8, param_2=b'\x00'*2):
-        self.conn.sendall(b'\x00' + command_num.to_bytes(1, 'big') + param_1 + param_2)
+        self.conn.sendall(b'\x01' + command_num.to_bytes(1, 'big') + param_1 + param_2)
 
     def run(self):
+        self.sock.listen()
+        self.conn, self.addr = self.sock.accept()
+
         with self.conn:
             print(f"Connected by {self.addr}, Username: {self.username}")
             while True:
                 initial_byte = self.conn.recv(1)
-                if int.from_bytes(initial_byte, 'big') == 1:
+                if initial_byte[0] == 1:
                     # recieve rest of command
                     command_bytes = self.conn.recv(12)
                     self.process_command(command_bytes)
@@ -97,7 +95,8 @@ class Server:
     PORT = 65432
 
     def __init__(self):
-        self.users = []
+        self.sockets = []
+        self.users: list[User] = []
 
     def run(self):
         print("Server is running!")
@@ -107,14 +106,21 @@ class Server:
             sock.listen()
 
             while True:
-                conn, addr = sock.accept()
+                conn, _ = sock.accept()
 
-                user = User(conn, addr)
+                new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                new_port = self.PORT + len(self.users) + 1
+                new_sock.bind((self.HOST, new_port))
+                conn.sendall(new_port.to_bytes(2, 'little'))
+
+                user = User(new_sock)
                 self.users.append(user)
                 user.start()
 
+    def get_available_users(self):
+        return ", ".join([user.username for user in self.users])
+
 def main():
-    global server
     server = Server()
     server.run()
 
